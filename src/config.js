a484d489558332fb081ed424acb173e53f64a7b2
wangziyu0305 <88227034+wangziyu01@users.noreply.github.com>
2022-07-31 20:23:05 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,8 +11,8 @@ const GITHUB = "https://github.com/wangziyu01"; // 右上角 Github 图标链接
 const BACKGROUND_IMG_URL = "https://api.dujin.org/bing/1920.php"; // 背景图片url，默认为必应每日图片（第三方接口）
 
 const SORT_DATA = [
-  { sortId: 1, title: "分类1" },
-  { sortId: 2, title: "分类2" }
+  { sortId: 1, title: "主页" },
+//   { sortId: 2, title: "分类2" }
 ]; // 网站分类列表，请按照API.md中'/getSort'接口中返回的data格式填写
 
 const RECORD_NUMBER = ""; // 网站备案号，留空不显示
@@ -21,35 +21,33 @@ const PAGES_DATA = [
   {
     pageId: 1,
     sortId: 1,
-    title: "主标题",
-    subtitle: "副标题",
+    title: "网站主页",
+    subtitle: "博客主页",
     url: "https://blog-test-psi-nine.vercel.app/",
     icon: ""
   },
-  {
-    pageId: 2,
-    sortId: 2,
-    title: "主标题",
-    subtitle: "副标题",
-    url: "https://blog-test-psi-nine.vercel.app/",
-    icon: ""
-  }
+//   {
+//     pageId: 2,
+//     sortId: 2,
+//     title: "主标题",
+//     subtitle: "副标题",
+//     url: "https://blog-test-psi-nine.vercel.app/",
+//     icon: ""
+//   }
 ]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
 
 const SLOGAN = [
   "欢迎拜访",
   "歡迎拜訪",
   "Welcome, my friend!",
-  "訪問へようこそ",
   "嗨，别来无恙",
   "不忘初心，一生浪漫",
-  "最近还好吗？",
   "流星，落花，萤火",
   "马车越空，晃荡越响",
-  "Welcome!"
+  "Welcome my blog!"
 ]; // 在Logo下方显示的标题
 
-const FOOTER_INFO = true; // 是否显示抽屉右下角的文字信息
+const FOOTER_INFO = false; // 是否显示抽屉右下角的文字信息
 
 export default {
   SERVE,
